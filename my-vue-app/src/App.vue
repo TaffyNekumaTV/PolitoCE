bc00c1844da61e804bf6d459cdb9566d01f5caaa
NecoraNyaru <s334608@studenti.polito.it>
2025-02-08 17:02:33 +0100

    fa

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* Optional: Add your styles here, or leave it empty */
+</style>
